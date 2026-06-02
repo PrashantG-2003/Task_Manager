8b8f806ab2451d42b1346a83d1265d56c3ff3f18
Prashant Gangwar <pranshant2003@gmail.com>
2026-06-02 17:48:42 +0530

    Update server.js
    port changes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import taskRoutes from './src/routes/taskRoutes.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Security and standard parsers
 app.use(cors());
